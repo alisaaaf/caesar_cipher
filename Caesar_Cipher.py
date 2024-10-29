d1c1c22ab2ae2ed065e93fc2342aa93c1fc489cc
alisaaaf <miidnsun@gmail.com>
2024-10-29 17:40:42 +0100

    improvements done

--- Caesar_Cipher.py
+++ Caesar_Cipher.py
@@ -1,18 +1,30 @@
-def caesar_cipher(text, shift, language, mode):
-    shift %= len(language)
+def caesar_cipher(text, shift, alphabet, mode):
+    shift %= len(alphabet[0])
+    result = []
 
-    for i in text:
-        if mode == "encryption":
-            new = (i + shift) % len(language)
-        elif mode == "encryption":
-            new = (i - shift) % len(language)
-        result.append(language[new])
+    for char in text:
+        if char in alphabet[0]:
+            i = alphabet[0].index(char)
+            if mode == "encryption":
+                new = (i + shift) % len(alphabet[0])
+            elif mode == "decryption":
+                new = (i - shift) % len(alphabet[0])
+            result.append(alphabet[0][new])
+        elif char in alphabet[1]:
+            i = alphabet[1].index(char)
+            if mode == "encryption":
+                new = (i + shift) % len(alphabet[1])
+            elif mode == "decryption":
+                new = (i - shift) % len(alphabet[1])
+            result.append(alphabet[1][new])
+        else: result.append(char)
 
     return ''.join(result)
 
-def main():
-    alphabet = input('Enter language (1: english (capital/small), 2: russian (capital/small) ')
-    mode = input('Enter mode (encryption/C): ')
+if __name__ == '__main__':
+
+    language = input('Enter language (english/russian) ')
+    mode = input('Enter mode (encryption/decryption): ')
     shift = int(input('Enter shift : '))
     text = input('Enter text : ')
 
@@ -21,16 +33,12 @@ def main():
     english_capital = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     english_small = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-    if alphabet == "russian capital":
-        language = russian_capital
-    elif alphabet == "russian small":
-        language = russian_small
-    elif alphabet == "english capital":
-        language = english_capital
-    elif alphabet == "english small":
-        language = english_small
+    if language == "russian":
+        alphabet = [russian_capital, russian_small]
+    elif language == "english":
+        alphabet = [english_capital, english_small]
     else:
         print("Language is incorrect")
 
-    result = caesar_cipher(text, shift, language, mode)
+    result = caesar_cipher(text, shift, alphabet, mode)
     print("Result:", result)
